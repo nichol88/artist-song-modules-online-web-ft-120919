69e24102cbba4bc1fcb5c8d2b308a8d03d376f34
Brian Nicholls <nichol88@gmail.com>
2019-12-14 07:08:39 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,12 +3,6 @@ module Findable
     self.class.all.detect{|a| a.name == name}
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
 
 end
