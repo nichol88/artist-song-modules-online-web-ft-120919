d68bed7a6962d92841b71a0a75f0ebd84afbdccb
Brian Nicholls <nichol88@gmail.com>
2019-12-14 08:10:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
+  extend Memorable
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
